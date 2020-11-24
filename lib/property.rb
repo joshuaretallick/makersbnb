39327523c91b95aa38d1eb68fe8072a469ad2196
Georgie Horth <georgiehorth@gmail.com>
2020-11-24 11:56:06 +0000

    add all method unit tests passes

--- lib/property.rb
+++ lib/property.rb
@@ -17,4 +17,11 @@ class Property
     Property.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], cost: result[0]['cost'].to_i)
   end
 
+  def self.all
+    result = DatabaseConnection.query("SELECT * FROM property")
+    result.map do
+      |property| Property.new(id: property['id'], name: property['name'], description: property['description'], cost: property['cost'].to_i)
+    end
+  end
+
 end
