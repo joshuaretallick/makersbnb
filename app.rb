93a152a5cd5edc2e40559291f1e46efa751dcb13
Joshua Retallick <josh@old-empire.co.uk>
2020-11-24 16:20:43 +0000

    Updated App.rb to add session routes

--- app.rb
+++ app.rb
@@ -3,18 +3,20 @@ require_relative './database_connection_setup.rb'
 require './lib/user'
 require './lib/property'
 # require 'sinatra-flash'
+# require 'uri'
 
 
 class Makersbnb < Sinatra::Base
+
+  enable :sessions
+  register Sinatra::Flash
 
   get '/' do
     erb :'signup'
   end
 
   post '/' do
-    p "Hey whats up?"
     User.create(email: params[:email], password: params[:password])
-    p "Hey Im number 2?"
     redirect '/property'
   end
 
@@ -31,6 +33,34 @@ class Makersbnb < Sinatra::Base
     Property.create(name: params['name'], description: params['description'], cost: params['cost'])
     redirect '/property'
   end
+
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  # post '/sessions' do
+  #   session[:profile_id] = User.authenticate(email: params[:email], password: params[:password])
+  #
+  #   if session[:profile_id]
+  #     @profile = session[:profile_id]
+  #     redirect('/peeps')
+  #   else
+  #     flash[:notice] = 'Please check your email or password.'
+  #     redirect('/sessions/new')
+  #   end
+  #
+  # end
+  #
+  # post '/sessions/destroy' do
+  #   session.clear
+  #   redirect('/')
+  # end
+  #START FROM HERE
+
+
+
+
+
 
 run! if app_file == $0
 end
