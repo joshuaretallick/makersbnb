354e336856c5a91847f68506d20df4d2dc16128b
savannaelbey <savanna_e@icloud.com>
2020-11-24 15:37:05 +0000

    sign up feature test, created User class, added create class method

--- app.rb
+++ app.rb
@@ -1,12 +1,23 @@
 require 'sinatra/base'
 require_relative './database_connection_setup.rb'
+require './lib/user'
 # require 'sinatra-flash'
 
 
 class Makersbnb < Sinatra::Base
+
   get '/' do
-  'Hello World'
-end
+    erb :'signup'
+  end
+
+  post '/' do
+    User.create(email: params[:email], password: params[:password])
+    redirect '/property'
+  end
+
+  get '/property' do
+    'Welcome to Makersbnb'
+  end
 
 run! if app_file == $0
 end
---
+++ spec/features/user/create_user_profile_spec.rb
@@ -0,0 +1,9 @@
+feature 'sign up to Makersbnb' do
+  scenario 'user can create a profile on Makersbnb if they dont already have one' do
+    visit '/'
+    fill_in('email', with: 'test@test.co.uk')
+    fill_in('password', with: 'passwordtest')
+    click_button('Sign up')
+    expect(page).to have_content('Welcome to Makersbnb')
+  end
+end
