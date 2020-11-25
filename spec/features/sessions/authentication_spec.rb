dfaceb4364b8ab66482bfede3a2f6cdf48cfc3cd
Joshua Retallick <josh@old-empire.co.uk>
2020-11-25 10:35:51 +0000

    Added tests to authentication

--- spec/features/sessions/authentication_spec.rb
+++ spec/features/sessions/authentication_spec.rb
@@ -6,6 +6,7 @@ feature 'authentication' do
     fill_in(:password, with: 'mario')
     click_button('Sign in')
     expect(current_path).to eq '/property'
+  #  expect(page).to have_content 'Welcome, mario@mario.com'
   end
 
   it 'a user can sign out' do
@@ -18,5 +19,25 @@ feature 'authentication' do
     expect(current_path).to eq '/'
     expect(page).to have_content 'You have successfully signed out'
   end
+
+  # it 'a user sees an error if they get their email wrong' do
+  #   User.create(email: 'mario@mario.com', password: 'mario')
+  #   visit '/sessions/new'
+  #   fill_in(:email, with: 'nottherightemail@me.com')
+  #   fill_in(:password, with: 'password123')
+  #   click_button('Sign in')
+  #   expect(page).not_to have_content 'Welcome, Mario Mario'
+  #   expect(page).to have_content 'Please check your email or password.'
+  # end
+  #
+  # it 'a user sees an error if they get their password wrong' do
+  #   User.create(email: 'mario@mario.com', password: 'mario')
+  #   visit '/sessions/new'
+  #   fill_in(:email, with: 'mario@mario.com')
+  #   fill_in(:password, with: 'password123')
+  #   click_button('Sign in')
+  #   expect(page).not_to have_content 'Welcome, Mario Mario'
+  #   expect(page).to have_content 'Please check your email or password.'
+  # end
 
 end
