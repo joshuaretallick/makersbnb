3b338821e5c62de39995ebf56f131c5512643cc5
Georgie Horth <georgiehorth@gmail.com>
2020-11-25 12:30:13 +0000

    .find method and unit tests passing

--- spec/features/booking_property_spec.rb
+++ spec/features/booking_property_spec.rb
@@ -3,7 +3,7 @@ feature 'user can book a property' do
     Property.create(name: "house", description: "small, 3 rooms", cost: 50)
     visit('/property')
     click_button('Request Booking')
-    # click_button('Confirm Booking')
+    click_button('Confirm Booking')
     expect(current_path).to eq '/property/confirmed'
     expect(page).to have_content 'Your booking is confirmed!'
   end
