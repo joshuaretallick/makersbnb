27bcee8d2206d112282cdd6b0b8cb7c6d45f7dc3
Joshua Retallick <josh@old-empire.co.uk>
2020-11-26 11:35:58 +0000

    Fixed setup test database to truncate booking table

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -6,5 +6,5 @@ def setup_test_database
   p "Setting up test database..."
   connection = PG.connect(dbname: 'makersbnb_test')
 
-  connection.exec("TRUNCATE users, property;")
+  connection.exec("TRUNCATE users, property, booking;")
 end
